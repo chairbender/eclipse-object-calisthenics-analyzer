7f87c888b587d28d6e1f4e50ce9ac89fafd2d02e
chairbender <kwhipke1@gmail.com>
2015-12-07 21:13:35 -0800

    Got all the dependencies figured out and running

--- eclipse_object_calisthenics_analyzer/src/com/chairbender/eclipse/object_calisthenics_analyzer/ObjectCalisthenicsAnalyzerHandler.java
+++ eclipse_object_calisthenics_analyzer/src/com/chairbender/eclipse/object_calisthenics_analyzer/ObjectCalisthenicsAnalyzerHandler.java
@@ -1,23 +1,15 @@
 package com.chairbender.eclipse.object_calisthenics_analyzer;
 
-import java.io.IOException;
-
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.handlers.HandlerUtil;
 
+import com.chairbender.eclipse.object_calisthenics_analyzer.views.CalisthenicsView;
 import com.chairbender.object_calisthenics_analyzer.ObjectCalisthenicsAnalyzer;
 import com.chairbender.object_calisthenics_analyzer.violation.ViolationMonitor;
-import com.github.javaparser.ParseException;
-
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.viewers.IStructuredSelection;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
@@ -35,35 +27,20 @@ public class ObjectCalisthenicsAnalyzerHandler extends AbstractHandler {
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		 IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-		    if (window != null)
-		    {
-		        IStructuredSelection selection = (IStructuredSelection) window.getSelectionService().getSelection();
-		        Object firstElement = selection.getFirstElement();
-		        if (firstElement instanceof IAdaptable)
-		        {
-		            IProject project = (IProject)((IAdaptable)firstElement).getAdapter(IProject.class);
-		            IPath path = project.getFullPath();
-		            
-		            try {
-						ViolationMonitor results = ObjectCalisthenicsAnalyzer.analyze(path.toFile(), "UTF-8");
-						results.getAllViolations();
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (ParseException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}		            		           
-		        }
-		    }
+	public Object execute(ExecutionEvent event) throws ExecutionException {		            
+        try {
+        	IPath workspacePath = ResourcesPlugin.getWorkspace().getRoot().getLocation();
+			ViolationMonitor results = ObjectCalisthenicsAnalyzer.analyze(workspacePath.toFile(), "UTF-8");
+			
+			//open up a new calisthenics view and display the results
+			CalisthenicsView calisthenicsView = (CalisthenicsView) PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().showView("com.chairbender.eclipse.object_calisthenics_analyzer.views.CalisthenicsView");
+			calisthenicsView.report(results);
+			results.getAllViolations();
+		} catch (Exception e) {
+			throw new ExecutionException("failed",e);
+		}
+		return null;		            		           
+    }
 		
-		
-		MessageDialog.openInformation(
-				window.getShell(),
-				"eclipse-object-calisthenics-analyzer",
-				"Hello, Eclipse world");
-		return null;
-	}
 }
+
